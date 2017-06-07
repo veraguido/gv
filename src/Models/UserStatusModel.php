42e791e30f2cab47e95d3e15905a3f10a8c43a6c
Guido Vera <guido.vera@smartbox.com>
2017-06-07 17:56:19 +0100

    adding userStatusmodel

---
+++ src/Models/UserStatusModel.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guido
+ * Date: 15/05/17
+ * Time: 11:24
+ */
+
+namespace Gvera\Models;
+
+/**
+ * Class UserStatusModel
+ * @package Gvera\Models
+ * @Entity @Table(name="user_status")
+ */
+class UserStatusModel
+{
+    /** @Id @Column(type="integer") @GeneratedValue  */
+    protected $id;
+
+    /** @Id @Column(type="string") */
+    protected $status;
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+}
