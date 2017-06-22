855c0f852da20ff9ade3e72bc17bfe389b6e7291
Guido Vera <guido.vera@smartbox.com>
2017-06-22 10:53:29 +0100

    adding exception for images on htaccess

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: guido
- * Date: 19/06/17
- * Time: 09:01
- */
 
 $this->addRoute('GET /examples/qwe/:car_id:/all');
 $this->addRoute('POST /examples/:page:/:car_id:/all');
