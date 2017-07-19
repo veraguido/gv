d2f62dce7aadc35980137e4c8b82ab5d272e9c83
Guido Vera <guido.vera@smartbox.com>
2017-07-19 17:40:56 +0100

    adding email wrapper

---
+++ src/Helpers/email/GVEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Gvera\Helpers\email\GVEmail;
+
+use Gvera\Helpers\config\Config;
+
+class GVEmail {
+
+    private $mailer;
+    private $host;
+    private $SMTPAuth = true;
+    private $username;
+    private $password;
+    private $SMTPSecure;
+    private $port;
+
+    public function __construct($isHtml, $subject, $body, $alternativeBody)
+    {
+        $config = Config::getInstance()->getConfig('email');
+        $this->mailer = new \PHPMailer();
+        $this->mailer->isHTML($isHtml);
+        $this->host = $config['host'];
+        $this->SMTPAuth = (bool)$config['smtp_auth'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->SMTPSecure = $config['smtp_secure'];
+        $this->port = $config['port'];
+
+        $this->mailer->Subject = $subject;
+        $this->mailer->Body = $body;
+        $this->mailer->AltBody = $alternativeBody;
+    }
+
+    public function addAddress($address, $name = '') {
+        $this->mailer->addAddress($address, $name);
+    }
+
+    public function addCC($cc, $name = '') {
+        $this->mailer->addCC($cc, $name);
+    }
+
+    public function addBcc($bcc, $name = '') {
+        $this->mailer->addBCC($bcc, $name);
+    }
+
+    public function addAttachment($path, $name = '', $encoding = 'base64', $type = '', $disposition = 'attachment') {
+        $this->mailer->addAttachment($path, $name, $encoding, $type, $disposition);
+    }
+
+    public function send(){
+        if(!$this->mailer->send()) {
+            throw new \Exception('Message could not be sent ' . $this->mailer->ErrorInfo);
+        } else {
+            //your implementation here
+        }
+    }
+
+}
