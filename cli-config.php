4ca53496fb88540759453b8bbe77a70ab13cf265
Guido Vera <vera.a.guido@gmail.com>
2018-12-17 13:08:56 +0000

    removing comment

--- cli-config.php
+++ cli-config.php
@@ -2,5 +2,4 @@
 
 require_once 'config/doctrine_boostrap.php';
 
-//return new \Symfony\Component\Console\Helper\HelperSet(array('em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)));
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
